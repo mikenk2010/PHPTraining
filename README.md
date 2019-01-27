025cef3e00ce66e26c2ef00229dc09102052c9b3
minhkhoidev <minhkhoi289@gmail.com>
2019-01-27 20:03:10 +0700

    edit exercise4

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
 # PHPTraining
+## Bài tập 1:
+Viết một trang nhập liệu và hiển thị nội dung.
+1. Page 1: Home page, liệt kê index (Layout: https://i.imgur.com/Mjl62hP.png)
+2. Page 2: Add new item (form nhập liệu)
+3 Page 3: View Item (html text, miễn sao render được nội dung lên là được)
+4. Page 4: Update item (cập nhận dữ liệu theo đúng item)
+5. Page 5: Delete (qua trang delete, bấm nút delete thì delete item)
+
+Note: Kết nối DB, cập nhật, thêm dữ liệu dzo Mysql
+
+Fields: 
+- First name (text field), 
+- Last Name (text field), 
+- Gender (Select), 
+- Address (Text Area), 
+- DoB (Select list)
+- Status (Bool)
+- CreatedDate (Datetime)
+
+## Bài tập 2: 
+1. Tạo một tài khoản trên github.com 
+2. Push project của mình lên đó, học dùng git, cách commit, cách add repo etc. 
+3. Làm thêm một table gọi là Roles, table dùng để gán quyền cho các User
+ 4. Làm một trang quản lý Role và quản lý User đang có role nào, quyền gì. 
+5. Khi tạo Employee thì gán Roles cho User này. 
+6. Ở trang quản lý Employee, nút Delete, chuyển thành Ajax button, khi bấm thì lên popup rồi delete Record đó. 
+Roles: ID (int) RoleName (Varchar 50) AllowAdd (Bool) AllowEdit (Bool) AllowView (Bool) AllowDelete (Bool) 
+RoleMapping: ID (int) EmployeeId (Int) - Ref tới table Employee ở bài trước RoleId (Int) - Ref tới table Roles
+
+## Bài tập 3 : 
+làm bài tập 2 theo framwork Codeigniter
+
+## Bài tập 4: Rest API
+Em viết thêm 1 controller trong CI để expose tất cả ra Rest API (JSON format)
+
+Rồi dùng jquery $.ajax để handle các action.
+
+- Index load bằng ajax
+- Add new: hiện popup form, khi add thì tự trong cái table index thêm cột luôn, ko cần refresh page.
+- Edit/Delete dủng hiện popup form: khi edit, delete thì page index tự update luôn, ko refresh.
+
+Tất cả xử lý bằng Ajax hết, ko dùng các server request nữa.
+
+Popup form giống như vậy https://i.imgur.com/NnsOv9W.png
+
+Có Blur background để các action thay đổi ở page index.
+
+## Bài tập 5:
+ - Thêm chức năng Send email, gửi email tới cho user sau khi tạo record, trong email có 1 activation link, khi click dzo link này thì update status của user. - Thêm chức năng export PDF, export user data ra PDF file.
+
+##  Bài tập 6: 
+- CHuyển chức năng của bt 5 thành Ajax. 
+
+## Bài tập 7: 
+- Deploy project lên AWS, để a tạo cho e account, rồi ssh key, rồi e access và upload project của mình lên đó
--- exercise4/application/controllers/Rest_User.php
+++ exercise4/application/controllers/Rest_User.php
@@ -17,35 +17,30 @@ class Rest_User extends REST_Controller
 
     public function users_get()
     {
-        $Data=$this->User_model->getUser();
-        $users=array();
-        for($i=1;$i<count($Data);$i++)
-        {
-            $users[]=[
-                'id'=>$i,
-                'FirstName'=>$Data[$i]['FirstName'],
-                'LastName'=>$Data[$i]['LastName'],
-                'Gender'=>$Data[$i]['Gender'],
-                'Address'=>$Data[$i]['Address'],
-                'DoB'=>$Data[$i]['DoB'],
-                'Avatar'=>$Data[$i]['Avatar'],
-                'Status'=>$Data[$i]['Status'],
-                'CreateDate'=>$Data[$i]['CreateDate']
+        $Data = $this->User_model->getUser();
+        $users = array();
+        for ($i = 1; $i < count($Data); $i ++) {
+            $users[] = [
+                'id' => $i,
+                'FirstName' => $Data[$i]['FirstName'],
+                'LastName' => $Data[$i]['LastName'],
+                'Gender' => $Data[$i]['Gender'],
+                'Address' => $Data[$i]['Address'],
+                'DoB' => $Data[$i]['DoB'],
+                'Avatar' => $Data[$i]['Avatar'],
+                'Status' => $Data[$i]['Status'],
+                'CreateDate' => $Data[$i]['CreateDate']
             ];
         }
-        $id = $this->get('id'); 
+        $id = $this->get('id');
         // If the id parameter doesn't exist return all the users
-        
-        if ($id === NULL)
-        {
+
+        if ($id === NULL) {
             // Check if the users data store contains users (in case the database result returns NULL)
-            if ($users)
-            {
+            if ($users) {
                 // Set the response and exit
                 $this->response($users, REST_Controller::HTTP_OK); // OK (200) being the HTTP response code
-            }
-            else
-            {
+            } else {
                 // Set the response and exit
                 $this->response([
                     'status' => FALSE,
@@ -53,55 +48,85 @@ class Rest_User extends REST_Controller
                 ], REST_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404) being the HTTP response code
             }
         }
-        
+
         // Find and return a single record for a particular user.
-        
+
         $id = (int) $id;
-        
+
         // Validate the id.
-        if ($id <= 0)
-        {
+        if ($id <= 0) {
             // Invalid id, set the response and exit.
             $this->response(NULL, REST_Controller::HTTP_BAD_REQUEST); // BAD_REQUEST (400) being the HTTP response code
         }
-        
+
         // Get the user from the array, using the id as key for retrieval.
         // Usually a model is to be used for this.
-        
+
         $user = NULL;
-        
-        if (!empty($users))
-        {
-            foreach ($users as $key => $value)
-            {
-                if (isset($value['id']) && $value['id'] === $id)
-                {
+
+        if (! empty($users)) {
+            foreach ($users as $key => $value) {
+                if (isset($value['id']) && $value['id'] === $id) {
                     $user = $value;
                 }
             }
         }
-        
-        if (!empty($user))
-        {
+
+        if (! empty($user)) {
             $this->set_response($user, REST_Controller::HTTP_OK); // OK (200) being the HTTP response code
-        }
-        else
-        {
+        } else {
             $this->set_response($users, REST_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404) being the HTTP response code
         }
     }
 
     public function users_delete()
     {
-        $First = $this->post('first');
-        $Last=$this->post('last');
-        $this->User_model->deleteUser($First, $Last);
+        $First = $this->query('FirstName');
+
+        $Last = $this->query('LastName');
+        $Flag = $this->User_model->deleteUser($First, $Last);
+        if ($Flag == TRUE) {
+            $this->response(array(
+                'mess' => $First,
+                'mess2' => $Last
+            ), REST_Controller::HTTP_OK);
+        } else
+            $this->response(array(
+                'mess' => 'fail',
+                'mess2' => 'try again'
+            ), REST_Controller::HTTP_BAD_REQUEST);
     }
-    
+
     public function users_post()
     {
-        //$this->User_model->updateUser();
+        $Data = array();
+        $Last = $this->post('LastName');
+        $First = $this->post('FirstName');
+        $Gender = $this->post('Gender');
+        $Address = $this->post('Address');
+        $Str = $this->post('DoB');
+        $Date = date_create_from_format('Y-m-d', $Str);
+        $Status = $this->post('Status');
+        $Data = [
+            'Gender' => $Gender,
+            'Address' => $Address,
+            'DoB' => $Date->format('Y-m-d'),
+            'Status' => $Status
+        ];
+        $Flag = $this->User_model->updateUser($First, $Last, $Data);
+        if ($Flag == TRUE)
+            $this->response(array(
+                'mess' => $Last
+            ), REST_Controller::HTTP_OK);
+        else
+            $this->response(array(
+                'mess' => FALSE
+            ), REST_Controller::HTTP_BAD_REQUEST);
+    }
+
+    public function users_put() {
+        $User=array();
     }
 }
-    
+
 ?>
